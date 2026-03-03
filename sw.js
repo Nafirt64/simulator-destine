f05a62ddab9d992683da35e1a91a2953b8e7a3eb
Nafirt64 <ofertele2020@gmail.com>
2026-03-03 13:08:41 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "destine-simulator-v3w-20260303";
+
+const CACHE_NAME = "destine-simulator-rot-20260303";
 self.addEventListener("install", e=>self.skipWaiting());
 self.addEventListener("activate", e=>self.clients.claim());
 self.addEventListener("fetch", e=>{});
